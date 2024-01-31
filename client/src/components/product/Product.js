44412f8460279cfa9413c5d0a86d71ce789850bf
anand <anandpromax@gmail.com>
2024-01-31 19:30:06 +0530

    first commit

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -3,35 +3,29 @@ import "./Product.scss";
 // @ts-ignore
 
 import { useNavigate } from "react-router-dom";
-import { ShimmerProduct } from "../shimmerCard/ShimmerProductPage";
-
 function Product({ product }) {
   const navigate = useNavigate();
   return (
-    <>
-      <div
-        className="Product"
-        onClick={() => navigate(`/products/${product.attributes?.key}`)}
-      >
-        <div className="product-container">
-          <div className="product-img">
-            <div className="img-container">
-              
-                <img
-                  src={product?.attributes.images?.data.attributes.url}
-                  alt={product?.attributes.title}
-                  id={product?.attributes.key}
-                />
-          
-            </div>
-          </div>
-          <div className="product-info">
-            <p className="title"> {product?.attributes?.title}</p>
-            <p className="price">₹{product?.attributes?.price}</p>
+    <div
+      className="Product"
+      onClick={() => navigate(`/products/${product.attributes?.key}`)}
+    >
+      <div className="product-container">
+        <div className="product-img">
+          <div className="img-container">
+            <img
+              src={product?.attributes.images?.data.attributes.url}
+              alt= "robo-img"
+              id= "img"
+            />
           </div>
         </div>
+        <div className="product-info">
+          <p className="title"> {product?.attributes?.title}</p>
+          <p className="price">₹{product?.attributes?.price}</p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
